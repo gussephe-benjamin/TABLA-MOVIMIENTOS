41dbecc09013e4a2ee97f3ab96088468edf20f9e
Gussephe_Benjamin <107523472+gussephe-benjamin@users.noreply.github.com>
2024-11-16 00:20:36 -0500

    Update registrarMovimiento.py

--- registrarMovimiento.py
+++ registrarMovimiento.py
@@ -41,7 +41,7 @@ def lambda_handler(event, context):
             if resultado == 'exitoso':
                 monto = abs(monto)  # Aseguramos que sea positivo
 
-        elif 'TABLA-SOLICITUDES' in event_source_arn:
+        elif 'TABLA-SOLICITUD-PRESTAMO' in event_source_arn:
             tipo_movimiento = 'solicitud'
             descripcion = "Solicitud creada"
             # Las solicitudes pueden no tener un impacto directo en el monto, así que lo dejamos como está
